f9441d40f6998a827416fec32846f363f6119b4f
Yondalne <benalicherif.yd@gmail.com>
2023-12-26 10:52:34 +0000

    add loading animation

--- lib/widgets/pages/RegisterScreen.dart
+++ lib/widgets/pages/RegisterScreen.dart
@@ -16,107 +16,106 @@ class RegisterScreen extends StatelessWidget {
   final AuthenticationController _authenticationController =
       Get.put(AuthenticationController());
 
-  void register(BuildContext context) async {
+  void register() async {
     await _authenticationController.register(
-      context: context,
-      name: nameController.text.trim(),
-      email: emailController.text.trim(),
-      password: passwordController.text.trim(),
-      contact: contactController.text.trim(),
-    );
+        name: nameController.text.trim(),
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+        contact: contactController.text.trim());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Welcome message
-              Text(
-                "Sign Up",
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  fontWeight: FontWeight.w700,
-                  fontSize: 30,
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // username textfield
-              MyTextField(
-                controller: nameController,
-                hintText: "Name",
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              // email textfield
-              MyTextField(
-                controller: emailController,
-                hintText: "Email",
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              // password textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 10),
-
-              // contact textfield
-              MyTextField(
-                controller: contactController,
-                hintText: "Contact",
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 50),
-
-              // signin button
-              Obx(() {
-                return MyButton(
-                  text: "Register",
-                  onTap: () => register(context),
-                );
-              }),
-
-              const SizedBox(height: 50),
-
-              // login
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Already has an account?"),
-                  SizedBox(
-                    width: 8,
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Welcome message
+                Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Colors.blue, fontWeight: FontWeight.w600),
+                ),
+
+                const SizedBox(height: 25),
+
+                // username textfield
+                MyTextField(
+                  controller: nameController,
+                  hintText: "Name",
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                // email textfield
+                MyTextField(
+                  controller: emailController,
+                  hintText: "Email",
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                // password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10),
+
+                // contact textfield
+                MyTextField(
+                  controller: contactController,
+                  hintText: "Contact",
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 50),
+
+                // signin button
+                Obx(() {
+                  return _authenticationController.isLoading.value
+                      ? CircularProgressIndicator()
+                      : MyButton(
+                          text: "Register",
+                          onTap: register,
+                        );
+                }),
+
+                const SizedBox(height: 50),
+
+                // login
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already has an account?"),
+                    SizedBox(
+                      width: 8,
                     ),
-                  )
-                ],
-              )
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.w600),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
